b76a5e6f6083d321a8bcd5fc6b471ac395bc4932
Eldar4432 <aibekov4432@gmail.com>
2023-02-03 20:15:22 +0600

    create empty index.html  array of questions

---
+++ script.js
@@ -0,0 +1,28 @@
+const quizData = [
+    {
+        question: 'How old is Eldar?',
+        a: '19',
+        b: '22',
+        c: '20',
+        d: '21',
+        correct: 'c'
+    }, {
+        question: 'What is the most used programming languages in 2022?',
+        a: 'JavaScript',
+        b: 'Python',
+        c: 'Java',
+        d: 'C#',
+        correct: 'a'
+    }, {
+        question: 'Who is president of Kyrgyzstan?',
+        a: 'Almazbek Atambaev',
+        b: 'Adahan Madumarov',
+        c: 'Sadyr Japarov',
+        d: 'Eldar Aibekov',
+        correct: 'c'
+    }, {
+        question: 'What does HTML stand for?',
+        a: 'Hypertext Markup Language',
+        b: 'Cascading Style Sheet'
+    }
+] 
